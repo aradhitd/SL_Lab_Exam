d537718c13a1ba0dea2f60b98ca613e9d83a55b4
aradhitd <aradhitd@gmail.com>
2020-12-10 18:10:01 +0530

    File Upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataFormComponent } from './data-form/data-form.component';
+import { RecordsComponent } from './records/records.component';
+import {MatTabsModule} from '@angular/material/tabs'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatDatepickerModule} from '@angular/material/datepicker'; 
+import { MatNativeDateModule } from '@angular/material/core';
+
+import { HttpClientModule } from '@angular/common/http';
+import {MatSelectModule} from '@angular/material/select'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataFormComponent,
+    RecordsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTabsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule ,
+    MatSelectModule,
+    HttpClientModule
+  ],
+  providers: [MatNativeDateModule ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/details.ts
@@ -0,0 +1,21 @@
+export const Details=[
+    {
+        date:'10/11/20',
+        tos:'arad',
+        wut:'5',
+        mood:"Good",
+    },
+    {
+        date:'12/11/20',
+        tos:'rohit',
+        wut:'10',
+        mood:"Average",
+    },
+    {
+        date:'13/11/20',
+        tos:'mime',
+        wut:'14',
+        mood:"Best",
+    },
+
+]
